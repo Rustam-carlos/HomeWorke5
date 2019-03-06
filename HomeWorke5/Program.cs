808cb4b20e5e41dd325df152f39b70c2370c1583
Rustam Galeev <Vincent-cat@mail.ru>
2019-03-06 15:12:29 +0600

    Добавьте файлы проекта.

---
+++ HomeWorke5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorke5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Создать индексатор для одномерного массива который при установке значения 
+            //    будет возводить в квадрат передаваемое значение переменной и устанавливать 
+            //    его для указанного индекса.При получении элемента массива по индексу будет 
+            //    возвращаться его текущее значение
+
+            Array array = new Array(10);
+            Random rnd = new Random();          
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+            }
+
+            for (int i = 0; i < array.Size; i++)
+            {
+                Console.WriteLine(i + "\t" + array[i]);
+            }  
+                       
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWorke5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HomeWorke5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWorke5")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4707edb4-5a86-4546-963c-eb8b1a53467c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
